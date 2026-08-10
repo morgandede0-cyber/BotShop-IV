ce30d55d3aaedb921319a27d609640a988361aad
morgandede0-cyber <morgandede0@gmail.com>
2026-08-10 18:25:04 +0200

    Update ShopIV.py

--- ShopIV.py
+++ ShopIV.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord import ui
 from discord.ext import commands
-import libsql_experimental as libsql
+import libsql
 
 intents = discord.Intents.default()
 intents.message_content = True
