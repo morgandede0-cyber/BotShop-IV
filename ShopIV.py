310502ad006d5a5eb8cf25a7b27fa94cc7e11842
morgandede0-cyber <morgandede0@gmail.com>
2026-08-10 22:42:56 +0200

    Update ShopIV.py

--- ShopIV.py
+++ ShopIV.py
@@ -10,7 +10,7 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Connexion à la base de données locale SQLite (simple et sans Turso)
-DB_PATH = "economy.db"
+DB_PATH = "/data/economy.db"
 
 def get_db_connection():
     return sqlite3.connect(DB_PATH)
