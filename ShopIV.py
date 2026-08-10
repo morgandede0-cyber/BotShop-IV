81255fd6c9901494daa7ce553639ab6a0ece391b
morgandede0-cyber <morgandede0@gmail.com>
2026-08-10 23:16:01 +0200

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- ShopIV.py
+++ ShopIV.py
@@ -6,6 +6,8 @@ import sqlite3
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+intents.presences = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
